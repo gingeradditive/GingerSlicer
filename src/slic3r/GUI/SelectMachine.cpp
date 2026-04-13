d047c7eedbc72aab269cbb43403e27e347e38ccc
Jack_up <36533859+GiacomoGuaresi@users.noreply.github.com>
2026-04-13 13:46:47 +0200

    chore: wrap SetTextColor string literals with StateColor or wxString constructors in SelectMachine and SendToPrinter dialogs

--- src/slic3r/GUI/SelectMachine.cpp
+++ src/slic3r/GUI/SelectMachine.cpp
@@ -288,7 +288,7 @@ SelectMachineDialog::SelectMachineDialog(Plater *plater)
     m_button_refresh = new Button(m_basic_panel, _L("Refresh"));
     m_button_refresh->SetBackgroundColor(m_btn_bg_enable);
     m_button_refresh->SetBorderColor(m_btn_bg_enable);
-    m_button_refresh->SetTextColor("#FFFFFE");
+    m_button_refresh->SetTextColor(StateColor(wxString("#FFFFFE")));
     m_button_refresh->SetSize(SELECT_MACHINE_DIALOG_BUTTON_SIZE);
     m_button_refresh->SetMinSize(SELECT_MACHINE_DIALOG_BUTTON_SIZE);
     m_button_refresh->SetCornerRadius(FromDIP(10));
@@ -478,7 +478,7 @@ SelectMachineDialog::SelectMachineDialog(Plater *plater)
     m_button_ensure = new Button(m_panel_prepare, _L("Send"));
     m_button_ensure->SetBackgroundColor(m_btn_bg_enable);
     m_button_ensure->SetBorderColor(m_btn_bg_enable);
-    m_button_ensure->SetTextColor("#FFFFFE");
+    m_button_ensure->SetTextColor(StateColor(wxString("#FFFFFE")));
     m_button_ensure->SetSize(SELECT_MACHINE_DIALOG_BUTTON_SIZE);
     m_button_ensure->SetMinSize(SELECT_MACHINE_DIALOG_BUTTON_SIZE);
     m_button_ensure->SetMinSize(SELECT_MACHINE_DIALOG_BUTTON_SIZE);
